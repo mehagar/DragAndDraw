42bb1a438b0fe93922d56678a271d0665096a7f8
mehagar <michaelehagar@gmail.com>
2015-04-12 10:26:15 -0500

    Finished ch. 32

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/Box.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by mehagar on 4/12/2015.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
